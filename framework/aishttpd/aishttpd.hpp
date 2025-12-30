3f4e9a920507f5e6e99811792f441ba20fa05d85
Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
2025-12-30 10:35:39 +0700

    Initial C++ framework
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

---
+++ framework/aishttpd/aishttpd.hpp
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025  Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
+ * Copyright (C) 2025  Ammar Faizi <ammarfaizi2@gnuweeb.org>
+ */
+#ifndef FRAMEWORK__AISHTTPD__AISHTTPD_HPP
+#define FRAMEWORK__AISHTTPD__AISHTTPD_HPP
+
+#include <libaishttpd/http.h>
+
+namespace aishttpd {
+
+class aishttpd {
+private:
+	struct ais_http_ctx http_ctx_;
+public:
+};
+
+} /* namespace aishttpd */
+
+#endif /* #ifndef FRAMEWORK__AISHTTPD__AISHTTPD_HPP */

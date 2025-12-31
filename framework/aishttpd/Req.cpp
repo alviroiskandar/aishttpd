43c739a79e469ceb6a0b9e20ed6d618f6f8d5482
Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
2025-12-31 10:57:12 +0700

    viro: Initial commit for my personal website
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

--- framework/aishttpd/Req.cpp
+++ framework/aishttpd/Req.cpp
@@ -7,6 +7,8 @@
 #include "Httpd.hpp"
 #include <stdexcept>
 #include <cstring>
+#include <sys/stat.h>
+#include <cerrno>
 
 namespace aishttpd {
 
@@ -15,6 +17,7 @@ void Req::showFile(Httpd *h, Req *hr, const std::string &file_path)
 	struct ais_file_table *ftb = &h->http_ctx_.file_table;
 	struct ais_http_req *req = hr->get_req();
 	struct ais_http_res *res = &req->res;
+	struct stat st;
 	char ext[32];
 	int r = 0;
 
@@ -38,6 +41,16 @@ void Req::showFile(Httpd *h, Req *hr, const std::string &file_path)
 	r = ais_http_res_add_hdr(res, "Content-Type", ais_http_get_mime_type(ext));
 	if (r)
 		throw std::bad_alloc();
+
+	if (fstat(res->body.file->fd, &st) == 0) {
+		char date[64];
+		struct tm tm;
+		gmtime_r(&st.st_mtim.tv_sec, &tm);
+		strftime(date, sizeof(date), "%a, %d %b %Y %H:%M:%S GMT", &tm);
+		r = ais_http_res_add_hdr(res, "Last-Modified", date);
+		if (r)
+			throw std::bad_alloc();
+	}
 }
 
 void Req::redirect(Httpd *h, Req *hr, const std::string &url)
